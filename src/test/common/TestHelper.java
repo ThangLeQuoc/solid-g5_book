03713ac039e5b6238f944a01fb7925bd3dd3e813
Minh Quốc Nguyễn <minh.nguyen@adnovum.vn>
2018-06-07 21:09:49 +0700

    fix value

--- src/test/common/TestHelper.java
+++ src/test/common/TestHelper.java
@@ -7,7 +7,7 @@ public class TestHelper {
 
 	public static final String MASTERING_JAVA = "Mastering Java";
 
-	public static final String MASTERING_JAVASCRIPT = "Mastering Java";
+	public static final String MASTERING_JAVASCRIPT = "Mastering JavaScript";
 
 	public static final String MASTERING_LINUX = "Mastering Linux";
 
